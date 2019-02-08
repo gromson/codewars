e117f183d2544933cd02ab21d2f358ccfdec1515
Roman Yudin <roman.iudin@check24.de>
2019-02-08 17:53:37 +0100

    budy pairs completed

--- buddy_pairs/solution.go
+++ buddy_pairs/solution.go
@@ -4,12 +4,35 @@ import "math"
 
 // the return is `nil` if there is no buddy pair
 func Buddy(start, limit int) []int {
+	checked := make(map[int]int)
 
 	for n := start; n <= limit; n++ {
+		m, mBuddy := 0, 0
 
+		if v, ok := checked[n]; ok {
+			m = v
+		} else {
+			m = sumDividers(n) - 1
+			checked[n] = m
+		}
+
+		if m <= n {
+			continue
+		}
+
+		if v, ok := checked[m]; ok {
+			mBuddy = v
+		} else {
+			mBuddy = sumDividers(m) - 1
+			checked[m] = mBuddy
+		}
+
+		if mBuddy == n {
+			return []int{n, m}
+		}
 	}
 
-	return []int{}
+	return nil
 }
 
 func findDividers(num int) []int {
@@ -23,10 +46,49 @@ func findDividers(num int) []int {
 	}
 
 	for n := sqrt; n >= 1; n-- {
-		if num%n == 0 && num/n != n {
+		if num%n == 0 && num/n != n && num/n != num {
 			res = append(res, num/n)
 		}
 	}
 
 	return res
+}
+
+func sumDividers(n int) int {
+	dividers := findDividers(n)
+	res := 0
+	for _, d := range dividers {
+		res += d
+	}
+
+	return res
+}
+
+// ------------
+
+func d(n int) int {
+	r, i := 1, 2
+
+	for i*i < n {
+		if n%i == 0 {
+			r += i + n/i
+		}
+		i++
+	}
+
+	if i*i == n {
+		r += i
+	}
+
+	return r
+}
+func Buddy2(start, limit int) []int {
+	for n := start; n <= limit; n++ {
+		m := d(n) - 1
+		if n == d(m)-1 && n < m {
+			return []int{n, m}
+		}
+	}
+
+	return nil
 }
--- buddy_pairs/solution_test.go
+++ buddy_pairs/solution_test.go
@@ -7,9 +7,42 @@ type findDividerStruct struct {
 	res []int
 }
 
+type buddyStruct struct {
+	start int
+	limit int
+	res   []int
+}
+
 var findDividersCases = []findDividerStruct{
-	{6, []int{1, 2, 3, 6}},
-	{25, []int{1, 5, 25}},
+	{6, []int{1, 2, 3}},
+	{25, []int{1, 5}},
+	{75, []int{1, 3, 5, 15, 25}},
+	{48, []int{1, 2, 3, 4, 6, 8, 12, 16, 24}},
+}
+
+var buddyTestCases = []buddyStruct{
+	{1071625, 1103735, []int{1081184, 1331967}},
+	{57345, 90061, []int{62744, 75495}},
+	{2693, 7098, []int{5775, 6128}},
+	{6379, 8275, nil},
+}
+
+func TestBuddy(t *testing.T) {
+	for _, c := range buddyTestCases {
+		res := Buddy(c.start, c.limit)
+		if !equal(res, c.res) {
+			t.Errorf("result %v, expected %v\n", res, c.res)
+		}
+	}
+}
+
+func TestBuddy2(t *testing.T) {
+	for _, c := range buddyTestCases {
+		res := Buddy2(c.start, c.limit)
+		if !equal(res, c.res) {
+			t.Errorf("result %v, expected %v\n", res, c.res)
+		}
+	}
 }
 
 func Test_findDividers(t *testing.T) {
@@ -24,6 +57,18 @@ func Test_findDividers(t *testing.T) {
 func Benchmark_findDividers(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		findDividers(1000000000)
+	}
+}
+
+func BenchmarkBuddy(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Buddy(buddyTestCases[0].start, buddyTestCases[0].limit)
+	}
+}
+
+func BenchmarkBuddy2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Buddy2(buddyTestCases[0].start, buddyTestCases[0].limit)
 	}
 }
 
