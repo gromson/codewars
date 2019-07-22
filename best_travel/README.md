d91f5d5b7844b828384c58436f3b609627b7c3f1
romson <gromson@gmail.com>
2019-07-22 23:27:24 +0200

    task incomplete

--- best_travel/README.md
+++ best_travel/README.md
@@ -1,3 +1,5 @@
+https://www.codewars.com/kata/best-travel/train/go
+
 John and Mary want to travel between a few towns A, B, C ...
 Mary has on a sheet of paper a list of distances between these towns.
 `ls = [50, 55, 57, 58, 60]`. John is tired of driving and he says to Mary that
--- best_travel/solution.go
+++ best_travel/solution.go
@@ -1,11 +1,62 @@
 package best_travel
 
+import "fmt"
+
 /*
 t - max distance
 k - number of towns
  */
 func ChooseBestSum(t, k int, ls []int) int {
-	return check(t, k, ls, 0, 0)
+	total := make([][]int, 0)
+	res := make([]int, 0, 3)
+	check(t, k, k, ls, 0, &res, &total)
+	fmt.Printf("%v\n", total)
+
+	max := -1
+
+	for _, set := range total {
+		s := sum(set)
+		if s > max && s <= t {
+			max = s
+		}
+	}
+	return max
+}
+
+func check(t, k, s int, ls []int, start int, res *[]int, total *[][]int) {
+	length := len(ls)
+	s--
+	if s < 0 {
+		return
+	}
+
+	end := length - s
+
+	for i := start; i < end; i++ {
+		*res = append(*res, ls[i])
+
+		//if len(*res) == k {
+		if s==0 {
+			res := make([]int,0)
+			for a:=0;a<k;a++ {
+				res = append(res, ls[i-a])
+				//*res = []int{}
+			}
+			*total = append(*total, res)
+		}
+
+		if k >= 0 {
+			check(t, k, s, ls, i+1, res, total)
+		}
+	}
+}
+
+func sum(set []int) int {
+	s := 0
+	for _, n := range set {
+		s += n
+	}
+	return s
 }
 
 func check_(t, k int, ls []int, start int) int {
@@ -38,7 +89,7 @@ func check_(t, k int, ls []int, start int) int {
 	return sum
 }
 
-func check(t, k int, ls []int, sum, start int) int {
+func check__(t, k int, ls []int, sum, start int) int {
 	length := len(ls)
 	max := 0
 	for i := start; i < length; i++ {
@@ -46,6 +97,10 @@ func check(t, k int, ls []int, sum, start int) int {
 			sum += ls[i]
 			t -= ls[i]
 			k--
+		}
+
+		if k > 0 && ls[i] > t {
+			continue
 		}
 
 		if k == 0 && ls[i] <= t+ls[i-1] {
@@ -56,16 +111,22 @@ func check(t, k int, ls []int, sum, start int) int {
 			}
 		}
 
-		max = check(t, k, ls, sum, i+1)
+		max = check__(t, k, ls, sum, i+1)
+		for j := i + 1; j < length; j++ {
+			res := check__(t, k, ls, sum, j)
+			if res > max {
+				max = res
+			}
+		}
 	}
 
 	if k > 0 {
 		return -1
 	}
 
-	if max > sum {
-		sum = max
+	if max < sum {
+		max = sum
 	}
 
-	return sum
+	return max
 }
