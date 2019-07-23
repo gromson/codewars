f1e6c0909b2630c5af8d3da63c5707b4938c98eb
romson <gromson@gmail.com>
2019-07-24 00:42:07 +0200

    twice_linear solution completed

--- twice_linear/solution.go
+++ twice_linear/solution.go
@@ -5,13 +5,6 @@ import (
 	"sort"
 )
 
-// [1, 3, 4, 7, 9, 10, 13, 15, 19, 21, 22, 27, ...]
-// 0. 3
-// 1. 7
-// 2. 15
-// 3. 31
-// 4. 63
-
 type Node struct {
 	Value  int
 	Parent *Node
@@ -24,7 +17,7 @@ func NewNode(value int, parent *Node) *Node {
 	return &Node{value, parent, nil, nil, make([]int, 0)}
 }
 
-func (n *Node) Grow(deep int) {
+func (n *Node) Build(deep int) {
 	if deep == 0 {
 		return
 	}
@@ -32,8 +25,8 @@ func (n *Node) Grow(deep int) {
 	n.Left = NewNode(n.Value*2+1, n)
 	n.Right = NewNode(n.Value*3+1, n)
 
-	n.Left.Grow(deep - 1)
-	n.Right.Grow(deep - 1)
+	n.Left.Build(deep - 1)
+	n.Right.Build(deep - 1)
 }
 
 func (n *Node) ToSlice() []int {
@@ -52,71 +45,56 @@ func (n *Node) iterate(s *[]int) {
 }
 
 func DblLinear(n int) int {
-	steps := int(math.Ceil(math.Log2(float64(n))))
+	if n == 0 {
+		return 1
+	}
+
+	steps := int(math.Ceil(math.Log2(float64(n))))+2
 
 	tree := NewNode(1, nil)
-	tree.Grow(steps)
+	tree.Build(steps)
 
 	slice := tree.ToSlice()
 	sort.Ints(slice)
 
-	res := make([]int, 0, n)
+	u := make([]int, 0, n)
 	for _, r := range slice {
-		if len(res) > 0 && res[len(res)-1] == r {
+		if len(u) > 0 && u[len(u)-1] == r {
 			continue
 		}
 
-		res = append(res, r)
+		u = append(u, r)
 	}
 
-	return res[n]
+	return u[n]
 }
 
-type Queue []int
+func DblLinear2(n int) int {
 
-func (q *Queue) push(i ...int) {
-	*q = append(*q, i...)
-	//sort.Ints(*q)
-}
-
-func (q *Queue) pop() int {
-	r := (*q)[0]
-	*q = (*q)[1:]
-	return r
-}
-
-func DblLinear_(n int) int {
+	// Code
 	u := []int{1}
-	q := make(Queue, 0)
+	i := 0
+	j := 0
 
-	x := 1
-	y, z := x*2+1, x*3+1
-	q.push(y, z)
+	var y int
+	var z int
 
-	for len(u) <= n*2 || len(q) > 0 {
-		x = q.pop()
+	for len(u) <= n {
+		y = 2*u[i] + 1
+		z = 3*u[j] + 1
 
-		if u[len(u)-1] == x {
-			continue
-		}
-
-		u = append(u, x)
-		y, z := x*2+1, x*3+1
-		if len(u) <= n*2 {
-			q.push(y, z)
+		if y < z {
+			u = append(u, y)
+			i++
+		} else if y > z {
+			u = append(u, z)
+			j++
+		} else {
+			u = append(u, y)
+			i++
+			j++
 		}
 	}
+	return u[len(u)-1]
 
-	sort.Ints(u)
-
-	res := make([]int, 0, n)
-	for _, r := range u {
-		if len(res) > 0 && res[len(res)-1] == r {
-			continue
-		}
-
-		res = append(res, r)
-	}
-
-	return res[n]
 }
--- twice_linear/solution_test.go
+++ twice_linear/solution_test.go
@@ -3,13 +3,13 @@ package twice_linear
 import "testing"
 
 var testCases = [][2]int{
-	//{0, 1},
-	//{2, 4},
-	//{5, 10},
-	//{50, 175},
-	//{100, 447},
+	{0, 1},
+	{2, 4},
+	{5, 10},
+	{50, 175},
+	{100, 447},
 	{500, 3355},
-	//{1000, 8488},
+	{1000, 8488},
 }
 
 func TestDblLinear(t *testing.T) {
@@ -26,3 +26,19 @@ func BenchmarkDblLinear(b *testing.B) {
 		DblLinear(10000)
 	}
 }
+
+func TestDblLinear2(t *testing.T) {
+	for _, test := range testCases {
+		res := DblLinear2(test[0])
+		if res != test[1] {
+			t.Errorf("key %d is expected to be %d, %d given", test[0], test[1], res)
+		}
+	}
+}
+
+func BenchmarkDblLinear2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		DblLinear2(10000)
+	}
+}
+
