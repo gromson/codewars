9c67579e2ebf90ea12b9d57afd922e3a93df15a0
Roman Yudin <roman.iudin@check24.de>
2019-07-23 10:50:33 +0200

    twice linear solved

--- twice_linear/solution.go
+++ twice_linear/solution.go
@@ -1,6 +1,6 @@
 package twice_linear
 
-import "math"
+import "sort"
 
 // [1, 3, 4, 7, 9, 10, 13, 15, 19, 21, 22, 27, ...]
 // 0. 3
@@ -9,6 +9,19 @@ import "math"
 // 3. 31
 // 4. 63
 
+type Queue []int
+
+func (q *Queue) push(i ...int) {
+	*q = append(*q, i...)
+	sort.Ints(*q)
+}
+
+func (q *Queue) pop() int {
+	r := (*q)[0]
+	*q = (*q)[1:]
+	return r
+}
+
 func DblLinear(n int) int {
 	u := buildSequence(n)
 	return u[n]
@@ -16,16 +29,32 @@ func DblLinear(n int) int {
 
 func buildSequence(n int) []int {
 	u := []int{1}
+	q := make(Queue, 0)
 
 	y, z := getYZ(1)
+	q.push(y, z)
 
-	steps := int(math.Ceil(math.Log2(float64(n)))) + 1
+	for len(u) <= n {
+		x := q.pop()
 
-	for s := 0; s < steps; s++ {
-		y1, z1, y2, z2 := addValue(&u, y, z, n)
-		addValue(&u, y1, y2, n)
-		y, z = z1, z2
+		if u[len(u)-1] == x {
+			continue
+		}
+
+		y, z := addValue(&u, x)
+
+		//if len(u) <= n {
+			q.push(y, z)
+		//}
 	}
+
+	//steps := int(math.Ceil(math.Log2(float64(n)))) + 1
+	//
+	//for s := 0; s <= steps; s++ {
+	//	y1, z1, y2, z2 := addValues(&u, y, z, n)
+	//	addValues(&u, y1, y2, n)
+	//	y, z = z1, z2
+	//}
 
 	return u
 }
@@ -36,12 +65,18 @@ func getYZ(x int) (y, z int) {
 	return
 }
 
-func addValue(u *[]int, x1, x2, n int) (y1, z1, y2, z2 int) {
-	if len(*u) <= n {
-		*u = append(*u, x1, x2)
-		y1, z1 = getYZ(x1)
-		y2, z2 = getYZ(x2)
-	}
+func addValues(u *[]int, x1, x2 int) (y1, z1, y2, z2 int) {
+	//if len(*u) <= n {
+	*u = append(*u, x1, x2)
+	y1, z1 = getYZ(x1)
+	y2, z2 = getYZ(x2)
+	//}
 
+	return
+}
+
+func addValue(u *[]int, x int) (y, z int) {
+	*u = append(*u, x)
+	y, z = getYZ(x)
 	return
 }
--- twice_linear/solution_test.go
+++ twice_linear/solution_test.go
@@ -2,7 +2,7 @@ package twice_linear
 
 import "testing"
 
-var testCases = [][2]int{{5, 10}}
+var testCases = [][2]int{{0, 1}, {2, 4}, {5, 10}, {50, 175}, {100, 447}, {500, 3355}, {1000, 8488}}
 
 func TestDblLinear(t *testing.T) {
 	for _, test := range testCases {
@@ -10,5 +10,11 @@ func TestDblLinear(t *testing.T) {
 		if res != test[1] {
 			t.Errorf("key %d is expected to be %d, %d given", test[0], test[1], res)
 		}
+	}
+}
+
+func BenchmarkDblLinear(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		DblLinear(testCases[6][0])
 	}
 }
